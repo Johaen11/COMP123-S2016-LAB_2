2b14073af36f7dd16e543fa32d130b1a91d0f7c7
johaen11 <johngnanakumar@gmail.com>
2016-06-18 20:24:52 -0400

    Added SayHello and ShowAge Method

---
+++ COMP123-S2016-LAB3/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_LAB2
+{
+     class Person
+    {
+        //PRIVATE INSTANCE VARIABLE++++++++++++++++++++++++++++++
+        private string _name;
+        private int _age;
+
+        //PUBLIC PROPERTY+++++++++++++++++++++++
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+
+
+        }
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+            set
+            {
+                this._age = value;
+            }
+
+
+        }
+
+        public  void SayHello()
+        {
+            Console.WriteLine("{0} says Hello",_name);
+        }
+        
+        public void ShowAge()
+        {
+            Console.WriteLine("{0} is {1} years old",Name,Age);
+        }
+    }
+}
--- COMP123-S2016-LAB3/Program.cs
+++ COMP123-S2016-LAB3/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace COMP123_S2016_LAB3
+namespace COMP123_S2016_LAB2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Person person = new Person();
+            person.Name = "Tom";
+           person.SayHello();
+            person.Age = 47;
+            person.ShowAge();   
+
         }
     }
 }
